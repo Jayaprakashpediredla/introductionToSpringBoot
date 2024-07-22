11ed0eec9554cbf6549dcdd113d7bde5d0ef5dba
Jayaprakashpediredla <pediredlajayaprakash@gmail.com>
2024-07-23 00:21:33 +0530

    First commit

---
+++ src/main/java/com/codingshuttle/prakash/week1Intro/introductionToSprintBoot/IntroductionToSprintBootApplication.java
@@ -0,0 +1,14 @@
+package com.codingshuttle.prakash.week1Intro.introductionToSprintBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSprintBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntroductionToSprintBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshuttle/prakash/week1Intro/introductionToSprintBoot/IntroductionToSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.prakash.week1Intro.introductionToSprintBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
