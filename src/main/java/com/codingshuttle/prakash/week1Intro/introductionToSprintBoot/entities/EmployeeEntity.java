21bd696d027572f20787bb83c3b93820f3ddbd21
Jayaprakashpediredla <pediredlajayaprakash@gmail.com>
2024-08-07 00:08:42 +0530

    Spring data jpa - intro

---
+++ src/main/java/com/codingshuttle/prakash/week1Intro/introductionToSprintBoot/entities/EmployeeEntity.java
@@ -0,0 +1,11 @@
+package com.codingshuttle.prakash.week1Intro.introductionToSprintBoot.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity {
+
+}
